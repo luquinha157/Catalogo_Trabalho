a0412f841a107718366cb9400665d19fd11b5598
lucas <lucasfariamelo307@gmail.com>
2023-12-10 10:41:37 -0300

    Adicionando mais jogos

--- catalogo/jogos/models.py
+++ catalogo/jogos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Jogo(models.Model):
-    nome = models.CharField(max_length=11)
+    nome = models.CharField(max_length=20)
     imagem = models.ImageField(upload_to='media/')
     descricao = models.TextField(max_length=150)
 
