ef3cdac2328c96cf214a855ed303106e97b6d6d3
lucas <lucasfariamelo307@gmail.com>
2023-12-10 14:01:56 -0300

    Adicionando usuarios

--- catalogo/setup/urls.py
+++ catalogo/setup/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('jogos.urls')),
+    path('', include('usuarios.urls')),
 ]
 
 if settings.DEBUG:
--- catalogo/usuarios/views.py
+++ catalogo/usuarios/views.py
@@ -1,3 +1,88 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from usuarios.forms import LoginForms, RegisterForms
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def index(request):
+    user = User.objects.all()
+    form = RegisterForms()
+    return render(request, 'usuarios/users.html', {'users': user, 'form':form})
+
+@login_required
+def dashboard(request):
+    return render(request, "dashboard/dashboard.html")
+
+def login(request):
+    form = LoginForms()
+
+    if request.method == 'POST':
+        form = LoginForms(request.POST)
+
+        if form.is_valid():
+            email = form['email'].value()
+            password = form['senha'].value()
+        user_temp = User.objects.get(email= email)
+
+        user = auth.authenticate(
+            request,
+            username=user_temp,
+            password=password
+        )
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, f'Foi logado com sucesso!')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Erro ao efetuar login')
+            return redirect('login')
+
+    return render(request, 'usuarios/login.html', {'form': form})
+
+@login_required
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForms(request.POST)
+
+        if form.is_valid():
+            name=form['name'].value()
+            email=form['email'].value()
+            password=form['password'].value()
+
+            if User.objects.filter(username=name).exists():
+                messages.error(request, 'Usuário já existente')
+                return redirect('users')
+
+            usuario = User.objects.create_user(
+                username=name,
+                email=email,
+                password=password
+            )
+            usuario.save()
+            messages.success(request, 'Usuario cadastrado com sucesso!')
+            return redirect('users')
+
+@login_required
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'Logout efetuado com sucesso!')
+    return redirect('login')
+
+@login_required
+def inative(request, id):
+    user = User.objects.get(id=id)
+    user.is_active = False
+    user.save()
+    messages.success(request, 'Usuario inativado com sucesso!')
+    return redirect('users')
+
+@login_required
+def active(request, id):
+    user = User.objects.get(id=id)
+    user.is_active = True
+    user.save()
+    messages.success(request, 'Usuario ativado com sucesso!')
+    return redirect('users')
