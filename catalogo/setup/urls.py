7676c662f95b74d8f83ad2988caf96d09c2fa26a
lucas <lucasfariamelo307@gmail.com>
2023-12-10 21:59:35 -0300

    Iniciando modal de login

--- catalogo/setup/urls.py
+++ catalogo/setup/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('jogos.urls')),
+    path('usuarios/', include('usuarios.urls')),
 ]
 
 if settings.DEBUG:
